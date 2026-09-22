12229c29707056b8df22a782b731a41d550a9158
kplevi05 <136925919+kplevi05@users.noreply.github.com>
2026-09-22 17:07:32 +0200

    release: set version to 2.0.3

--- android/public-app/build.gradle.kts
+++ android/public-app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "hu.orszembejelento.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "2.0.2"
+        versionCode = 4
+        versionName = "2.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
